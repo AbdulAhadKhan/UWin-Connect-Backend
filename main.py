99a7948c82ef2485182cc7af89459aedd69fb6fa
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-01-26 23:03:58 -0500

    Cleanup and added gitignore

--- main.py
+++ main.py
@@ -2,26 +2,6 @@ from fastapi import FastAPI, Request
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "HelloWorld"}
-
-
-@app.get("/login/sherin")
-def about():
-    return {"message": "Password"}
-
-
-loginpage = {
-    1: {
-        "name": "Cherry",
-        "student_id": 3536333,
-        "program": "MAC"
-    }
-}
-
-
 @app.get("/echo")
 def get_item(request: Request):
     print(request.headers)
