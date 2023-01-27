a3366fb1350c1df242258b86f6197b33ed724389
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-01-26 23:08:25 -0500

    Fixed echo to respond with incoming JSON object

--- main.py
+++ main.py
@@ -3,9 +3,5 @@ from fastapi import FastAPI, Request
 app = FastAPI()
 
 @app.get("/echo")
-def get_item(request: Request):
-    print(request.headers)
-    args = request.query_params.keys()
-    for key in args:
-        print(key, ":", request.query_params[key])
-    return str(request.headers) + str(args)
+async def get_item(request: Request):
+    return await request.json()
