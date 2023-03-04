28fe83e36c8ba4ce1f0462f064a3b5ec2aad1b7a
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 11:14:31 -0500

    Modified registration request model

--- models/user.py
+++ models/user.py
@@ -1,6 +1,19 @@
 from pydantic import BaseModel
+from typing import Union
 
 class User(BaseModel):
     userid: str
     email: str
     password: str
+
+class Faculty(BaseModel):
+    department: str
+    designation: str
+
+class Registration(BaseModel):
+    email: str
+    password: str
+    firstname: str
+    lastname: str
+    role: Union[str, Faculty]
+    gender: str
