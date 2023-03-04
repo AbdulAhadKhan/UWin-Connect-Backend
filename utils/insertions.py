ee3f18318f5fade18ae1074292dc46808c6b4e16
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 16:57:49 -0500

    Added feature to push session information to database

--- utils/insertions.py
+++ utils/insertions.py
@@ -1,7 +1,20 @@
 from ._utils import get_collection, hash_password
+from ._utils import generate_session_id
 
 def insert_user(user_info):
     """!@brief Insert a new user into the database"""
     collection = get_collection("users")
     user_info.password = hash_password(user_info.password)
     collection.insert_one(user_info.dict())
+
+
+def insert_session(user_id, session_info) -> str:
+    """!@brief Insert a new session into the database
+        @return session_id"""
+    collection = get_collection("sessions")
+    session_id = generate_session_id(user_id, session_info.login_time, session_info.machine_id)
+    collection.insert_one({"session_id": session_id, 
+                           "user_id": user_id, 
+                           "login_time": session_info.login_time, 
+                           "machine_id": session_info.machine_id})
+    return session_id
