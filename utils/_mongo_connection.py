aab4f2adcfb10fec320b464632045221ca29630c
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 14:47:29 -0500

    Removed artifact file

--- utils/_mongo_connection.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from pymongo import MongoClient
-
-CONFIGS = yaml.safe_load(open("utils/dev-config.yml"))
-
-def get_collection(collection_name) -> MongoClient:
-    assert collection_name in CONFIGS['collections'], f'Collection "{collection_name}" not found'
-    client = MongoClient(CONFIGS['atlas_uri'])
-    return client[CONFIGS['db']][CONFIGS['collections'][collection_name]]
