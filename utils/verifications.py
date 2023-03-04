73921ac8007c7f8eee48dcc7df9524ccac36d472
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 14:25:45 -0500

    Fixed typo

--- utils/verifications.py
+++ utils/verifications.py
@@ -2,5 +2,5 @@ from ._mongo_connection import get_collection
 
 def email_exists(email) -> bool:
     """!@brief Check if email exists in the database"""
-    collection = get_collection("user")
+    collection = get_collection("users")
     return collection.find_one({"email": email}) is not None
