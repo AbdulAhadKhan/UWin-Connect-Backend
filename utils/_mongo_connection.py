f08c25791072645033d2a95ce41125bd3d674e1e
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 14:25:22 -0500

    Migrated to yaml for better configs

--- utils/_mongo_connection.py
+++ utils/_mongo_connection.py
@@ -1,11 +1,9 @@
-import os
+import yaml
 from pymongo import MongoClient
 
-COLLECTION_MAP = {
-    "user": "USER_COLLECTION",
-}
+CONFIGS = yaml.safe_load(open("utils/dev-config.yml"))
 
 def get_collection(collection_name) -> MongoClient:
-    assert collection_name in COLLECTION_MAP, f'Collection "{collection_name}" not found'
-    client = MongoClient(os.environ.get("ATLAS_URI"))
-    return client[os.environ.get("DB")][os.environ.get(COLLECTION_MAP[collection_name])]
+    assert collection_name in CONFIGS['collections'], f'Collection "{collection_name}" not found'
+    client = MongoClient(CONFIGS['atlas_uri'])
+    return client[CONFIGS['db']][CONFIGS['collections'][collection_name]]
