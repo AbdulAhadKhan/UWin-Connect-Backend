fdfd88f8e87edd2e3ef172490c495f3844b8dd72
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 16:57:07 -0500

    Added util for generating session id

--- utils/_utils.py
+++ utils/_utils.py
@@ -1,10 +1,11 @@
 import yaml
 from argon2 import PasswordHasher, exceptions
+from hashlib import sha256
 from pymongo import MongoClient
 
 CONFIGS = yaml.safe_load(open("utils/dev-config.yml"))
 
-def get_collection(collection_name) -> MongoClient:
+def get_collection(collection_name: str) -> MongoClient:
     assert collection_name in CONFIGS['collections'], f'Collection "{collection_name}" not found'
     client = MongoClient(CONFIGS['atlas_uri'])
     return client[CONFIGS['db']][CONFIGS['collections'][collection_name]]
@@ -21,3 +22,9 @@ def compare_hashed_password(password: str, hashed_password: str) -> bool:
         return password_hasher.verify(hashed_password, password)
     except exceptions.VerifyMismatchError:
         return False
+    
+def generate_session_id(user_id, login_time: str, machine_id: str) -> str:
+    """!@brief Create a new session for a user
+        @return session_id"""
+    session_id = sha256(f"{user_id}{login_time}{machine_id}".encode('utf-8')).hexdigest()
+    return session_id
