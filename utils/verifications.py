f64e0bf4b5a2c110f34e9043ee6adafff6e96608
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-04 13:50:22 -0500

    Refactored for moved generic connection

--- utils/verifications.py
+++ utils/verifications.py
@@ -1,15 +1,6 @@
-import os
-from pymongo import MongoClient
-
-COLLECTION_MAP = {
-    "user": "USER_COLLECTION",
-}
-
-def get_collection(collection_name) -> MongoClient:
-    assert collection_name in COLLECTION_MAP, f'Collection "{collection_name}" not found'
-    client = MongoClient(os.environ.get("ATLAS_URL"))
-    return client[os.environ.get("DB")][os.environ.get("USER_COLLECTION")]
+from _mongo_connection import get_collection
 
 def email_exists(email) -> bool:
+    """!@brief Check if email exists in the database"""
     collection = get_collection("user")
     return collection.find_one({"email": email}) is not None
