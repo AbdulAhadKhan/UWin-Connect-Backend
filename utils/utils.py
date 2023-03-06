4dd3ebb1a202b941aefdfd2f33a04ce393b53cb4
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-06 13:04:05 -0500

    Refactored file name for global access

---
+++ utils/utils.py
@@ -0,0 +1,30 @@
+import yaml
+from argon2 import PasswordHasher, exceptions
+from hashlib import sha256
+from pymongo import MongoClient
+
+CONFIGS = yaml.safe_load(open("utils/dev-config.yml"))
+
+def get_collection(collection_name: str) -> MongoClient:
+    assert collection_name in CONFIGS['collections'], f'Collection "{collection_name}" not found'
+    client = MongoClient(CONFIGS['atlas_uri'])
+    return client[CONFIGS['db']][CONFIGS['collections'][collection_name]]
+
+def hash_password(password: str) -> str:
+    """!@brief Hash a password using Argon2."""
+    password_hasher = PasswordHasher()
+    return password_hasher.hash(password)
+
+def compare_hashed_password(password: str, hashed_password: str) -> bool:
+    """!@brief Verify a password using Argon2."""
+    password_hasher = PasswordHasher()
+    try:
+        return password_hasher.verify(hashed_password, password)
+    except exceptions.VerifyMismatchError:
+        return False
+    
+def generate_session_id(user_id, login_time: str, machine_id: str) -> str:
+    """!@brief Create a new session for a user
+        @return session_id"""
+    session_id = sha256(f"{user_id}{login_time}{machine_id}".encode('utf-8')).hexdigest()
+    return session_id
