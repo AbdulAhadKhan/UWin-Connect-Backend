3b459a436e7ff88aa40fd8d0398a74645f5aa7c0
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-06 13:06:34 -0500

    Added model for session

--- models/session.py
+++ models/session.py
@@ -3,3 +3,9 @@ from pydantic import BaseModel
 class NewSession(BaseModel):
     login_time: int
     machine_id: str
+
+class Session(BaseModel):
+    session_id: str
+    user_id: str
+    login_time: int
+    machine_id: str
