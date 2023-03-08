243cee4581b04a140be300bbda87a0420dd82769
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-08 16:37:01 -0500

    Added CORS support for frontend on localhost

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import Request
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routes.user import user_router
 
 app = FastAPI()
 app.include_router(user_router)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/echo")
 async def echo(request: Request):
