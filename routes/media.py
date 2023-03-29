df395c359d0421828b7da05f521c78d27103237c
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-28 23:03:52 -0400

    Get image checks if image exists and return 404 if if doesn't otherwise returns the image

--- routes/media.py
+++ routes/media.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+import os
+from fastapi import APIRouter, HTTPException
 from fastapi.responses import FileResponse
 
 media_router = APIRouter()
 
-@media_router.get("/get-image/{image}", status_code=200, 
-                  responses={200: {"message": "Image retrieved successfully"}},
+
+@media_router.get("/get-image/{image}", status_code=200,
+                  responses={200: {"message": "Image retrieved successfully"},
+                             404: {"message": "Image not found"}},
                   response_class=FileResponse)
 async def get_image(image: str):
-    return f".data/{image}"
+    file = f".data/{image}"
+    if os.path.exists(file):
+        return FileResponse(file)
+    raise HTTPException(status_code=404, detail="Image not found")
