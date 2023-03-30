06f7e83ea6ff14ed40a82ded903f6bcc20a8e67f
Abdul Ahad Khan <abdul.ahad.khan@live.com>
2023-03-29 20:16:11 -0400

    Storing image in AWS S3

--- routes/media.py
+++ routes/media.py
@@ -1,16 +1,20 @@
-import os
+import io
 from fastapi import APIRouter, HTTPException
-from fastapi.responses import FileResponse
+from fastapi.responses import Response
+from utils.utils import get_file
+import boto3
 
 media_router = APIRouter()
 
 
-@media_router.get("/get-image/{image}", status_code=200,
+@media_router.get("/get-image/{image_name}", status_code=200,
                   responses={200: {"message": "Image retrieved successfully"},
                              404: {"message": "Image not found"}},
-                  response_class=FileResponse)
-async def get_image(image: str):
-    file = f".data/{image}"
-    if os.path.exists(file):
-        return FileResponse(file)
-    raise HTTPException(status_code=404, detail="Image not found")
+                  response_class=Response)
+async def get_image(image_name: str, response: Response):
+    image = io.BytesIO(await get_file(image_name))
+    headers = {
+        "Content-Type": 'image/*',
+        "Content-Disposition": "inline",
+    }
+    return Response(content=image.getvalue(), headers=headers)
